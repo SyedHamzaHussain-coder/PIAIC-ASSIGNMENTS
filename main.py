30314c6fbd2434cb2ae8df3bae6a5261b49655ed
Syed Hamza Hussain <51454173+SyedHamzaHussain-coder@users.noreply.github.com>
2019-07-16 13:31:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ avg=(subject1+subject2+subject3+subject4+subject5)/5
 
 if(avg>=90):
     print("Grade: A")
-elif(avg>=80andavg<90):
+elif(avg>=80 and avg<90):
     print("Grade: B")
-elif(avg>=70andavg<80):
+elif(avg>=70 and avg<80):
     print("Grade: C")
-elif(avg>=60andavg<70):
+elif(avg>=60 and avg<70):
     print("Grade: D")
 
 else:
